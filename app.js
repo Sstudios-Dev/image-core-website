0aa7d80c5a7c401ec49c9341b15bdf4070829fad
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-04-20 17:14:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -52,5 +52,5 @@ app.get('/', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
