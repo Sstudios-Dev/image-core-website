5d99e17aec1173d84898e4bd475d1af1f87a7edc
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-04-20 15:26:37 -0600

    add - github Api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -7,8 +9,47 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index');
+async function countFilesInRepository(owner, repo, path) {
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+
+    try {
+        const response = await axios.get(apiUrl);
+        return response.data.length;
+    } catch (err) {
+        console.error('Error fetching repository contents:', err);
+        throw err;
+    }
+}
+
+async function countFilesAnime(ownerA, repoA, pathA) {
+    const apiUrl = `https://api.github.com/repos/${ownerA}/${repoA}/contents/${pathA}`;
+
+    try {
+        const response = await axios.get(apiUrl);
+        return response.data.length;
+    } catch (err) {
+        console.error('Error fetching repository contents:', err);
+        throw err;
+    }
+}
+
+app.get('/', async (req, res) => {
+    const owner = 'Sstudios-Dev';
+    const repo = 'image-core';
+    const path = 'src/img';
+
+    const ownerA = 'Sstudios-Dev';
+    const repoA = 'image-core';
+    const pathA = 'src/img-anime';
+
+    try {
+        const totalFiles = await countFilesInRepository(owner, repo, path);
+        const totalFilesAnime = await countFilesAnime(ownerA, repoA, pathA);
+        res.render('index', { totalFiles, totalFilesAnime });
+    } catch (err) {
+        console.error('Error counting files in repository:', err);
+        res.status(500).send('Error counting files');
+    }
 });
 
 const PORT = process.env.PORT || 3000;
